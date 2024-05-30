2b35c2454bfb9d26b9428f9d6b8eddcb86d9fa84
Matteo <matteo@agence-88.fr>
2024-05-30 17:42:10 +0200

    create - create - edit - toggle - delete

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -6,6 +6,7 @@ use App\Entity\Task;
 use App\Form\Type\TaskType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,10 +14,10 @@ use Symfony\Component\Routing\Attribute\Route;
 class TaskController extends AbstractController
 {
     #[Route('/tasks', name: 'task_list')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
-        return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+        return $this->render('task/list.html.twig', [
+            'tasks' => $entityManager->getRepository(Task::class)->findAll(),
         ]);
     }
 
@@ -38,5 +39,56 @@ class TaskController extends AbstractController
         }
 
         return $this->render('task/create.html.twig', ['form' => $form->createView()]);
+    }
+
+
+    #[Route(path: '/tasks/{id}/edit', name: 'task_edit')]
+    public function editAction(Task $task, Request $request,EntityManagerInterface $entityManager): RedirectResponse|Response
+    {
+        $form = $this->createForm(TaskType::class, $task);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La tâche a bien été modifiée.');
+
+            return $this->redirectToRoute('task_list');
+        }
+
+        return $this->render('task/edit.html.twig', [
+            'form' => $form->createView(),
+            'task' => $task,
+        ]);
+    }
+
+
+    #[Route(path: '/tasks/{id}/toggle', name: 'task_toggle')]
+    public function toggleTaskAction(Task $task,EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $task->setDone(!$task->isDone());
+        $entityManager->flush();
+
+        if ($task->isDone()) {
+            $this->addFlash('success', sprintf('La tâche %s a bien été marquée comme faite.', $task->getTitle()));
+        }else{
+            $this->addFlash('notice', sprintf('La tâche %s a bien été rajouter à la liste des tâches', $task->getTitle()));
+
+        }
+
+
+        return $this->redirectToRoute('task_list');
+    }
+
+    #[Route(path: '/tasks/{id}/delete', name: 'task_delete')]
+    public function deleteTaskAction(Task $task,EntityManagerInterface $entityManager): RedirectResponse
+    {
+        $entityManager->remove($task);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'La tâche a bien été supprimée.');
+
+        return $this->redirectToRoute('task_list');
     }
 }
